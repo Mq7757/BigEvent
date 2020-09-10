317c96a3354f706d37a436745ad802905603ddcc
Mq7757 <591875268@qq.com>
2020-09-10 15:28:11 +0800

    基本完成注册和提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -54,4 +54,24 @@ $(function() {
             })
     })
 
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败!')
+                }
+                layer.msg('登录成功!')
+                    // 将登录成功得到的 token 字符串,保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
 })
