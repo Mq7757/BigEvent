c469794a8223ce14aa3e1c5348ad59296e75b7a9
Mq7757 <591875268@qq.com>
2020-09-10 23:50:54 +0800

    完成登录注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,4 @@
 $(function() {
-    $('.reg-box').hide()
 
     // 点击去注册账号链接
     $('#link_reg').on('click', function() {
@@ -36,22 +35,43 @@ $(function() {
 
     // 监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
+            e.preventDefault()
+            var data = {
+                username: $('#form_reg [name=username]').val(),
+                password: $('#form_reg [name=password]').val()
+            }
+            $.post('http://ajax.frontend.itheima.net/api/reguse', data,
+                function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg(res.message)
+                            // console.log(res.message)
+                    }
+                    // console.log('注册成功');
+                    layer.msg('注册成功,请登录!')
+                        // 模拟点击行为
+                    $('#link_login').click()
+                })
+        })
+        // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        // 阻止默认提交行为
         e.preventDefault()
-        var data = {
-            username: $('#form_reg [name=username]').val(),
-            password: $('#form_reg [name=password]').val()
-        }
-        $.post('http://--------', data,
-            function(res) {
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg(res.message)
-                        // console.log(res.message)
+                    return layer.msg('登录失败！')
                 }
-                // console.log('注册成功');
-                layer.msg('注册成功,请登录!')
-                    // 模拟点击行为
-                $('#link_login').click()
-            })
+                layer.msg('登录成功！')
+                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                    // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
     })
 
 })
