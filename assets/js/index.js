8e34f5c468f6046ddd15339de2cfc6ce3f51d313
Mq7757 <591875268@qq.com>
2020-09-11 13:54:48 +0800

    针对接口做了些小修改

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,29 @@
-// 获取用户的基本信息
-function getUserInfo() {
-    $.ajax({
-        method: 'GET',
-        url: '/my/userinfo',
-        // headers 就是请求头配置对象
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
-        success: function(res) {
-            if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败！')
+$(function() {
+
+    getUserInfo()
+        // 获取用户的基本信息
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            // headers 就是请求头配置对象
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || ''
+            // },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败！')
+                }
+                // 调用 renderAvatar 渲染用户的头像
+                renderAvatar(res.data)
             }
-            // 调用 renderAvatar 渲染用户的头像
-            renderAvatar(res.data)
-        }
-    })
-}
+        })
+    }
+})
+
+
+
+
 // 渲染用户的头像
 function renderAvatar(user) {
     // 1. 获取用户的名称
@@ -57,31 +65,13 @@ $('#btnLogout').on('click', function() {
 
 // 不论成功还是失败，最终都会调用 complete 回调函数
 complete: function(res) {
-        // console.log('执行了 complete 回调：')
-        // console.log(res)
-        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            // 1. 强制清空 token
-            localStorage.removeItem('token')
-                // 2. 强制跳转到登录页面
-            location.href = '/login.html'
-        }
+    // console.log('执行了 complete 回调：')
+    // console.log(res)
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        // 1. 强制清空 token
+        localStorage.removeItem('token')
+            // 2. 强制跳转到登录页面
+        location.href = '/login.html'
     }
-    // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-    // 会先调用 ajaxPrefilter 这个函数
-    // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-    ...
-    // 全局统一挂载 complete 回调函数
-    options.complete = function(res) {
-        // console.log('执行了 complete 回调：')
-        // console.log(res)
-        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            // 1. 强制清空 token
-            localStorage.removeItem('token')
-                // 2. 强制跳转到登录页面
-            location.href = '/login.html'
-        }
-    }
-})
+}
